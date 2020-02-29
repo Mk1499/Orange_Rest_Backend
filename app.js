b2b5408b9f8aa4f68e4d50b922ebfe79639d1aea
MK1499 <m.khaled1499@gmail.com>
2020-02-29 23:04:51 +0200

    allow  cors

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ require("dotenv").config();
 
 // ======================================= MiddleWares ====================================//
 
-// app.use(cors());
+ app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
