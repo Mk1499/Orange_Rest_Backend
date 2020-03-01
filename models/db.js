a50f1ffd1796ad4778b689d34d309050e01d325a
MK1499 <m.khaled1499@gmail.com>
2020-03-01 19:57:44 +0200

    try to solve crash  2

--- models/db.js
+++ models/db.js
@@ -12,6 +12,23 @@ const connection = mysql.createConnection({
   database: dbConfig.DB
 });
 
+let handleDisconnect= (client) => {
+  client.on('error', function (error) {
+    if (!error.fatal) return;
+    if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
+
+    console.error('> Re-connecting lost MySQL connection: ' + error.stack);
+
+    // NOTE: This assignment is to a variable from an outer scope; this is extremely important
+    // If this said `client =` it wouldn't do what you want. The assignment here is implicitly changed
+    // to `global.mysqlClient =` in node.
+    mysqlClient = mysql.createConnection(client.config);
+    handleDisconnect(mysqlClient);
+    mysqlClient.connect();
+  });
+}
+
+handleDisconnect(connection); 
 // open the MySQL connection
 connection.connect(error => {
   if (error) console.log(error);
